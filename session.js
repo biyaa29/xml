dc0d66b7d9d50cc0750faad0f60344929d0e49f5
laiba2007 <145976451+laiba2007@users.noreply.github.com>
2024-01-24 13:03:19 +0500

    add new files

---
+++ session.js
@@ -0,0 +1,55 @@
+// let student="pass"
+// if(student=="pass"){
+//     document.write(true)
+// }
+// else{
+//     document.write(fale)
+// }
+
+// if else
+var success="yes"
+if(success=="yes"){
+    document.write(true)
+}
+else{
+    document.write(false)
+}
+
+//  for loop
+for(i=2;i<=15;i++){
+    document.write("<br>"+ "rabiyaa" +"<br>")
+}
+
+// while loop
+i=0;
+while(i<=10){
+    console.log(i)
+    i++;
+}
+
+// dowhile loop
+sehar=3;
+do{
+    console.log(sehar);
+    sehar++;
+}while(sehar<=8)
+// functions
+function  multiply(a,b,c){
+    console.log(a*b*c)
+}
+multiply(2,4,5)
+// function
+function name(name){
+    console.log(name)
+}
+name("rabiya")
+name("sehar")
+name("malyka")
+name("areeba")
+
+// prompt
+var name=prompt("enter your name")
+function username(username){
+    console.log("welcome"   + username)
+}
+username(name)
