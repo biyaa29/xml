f5cae163be158c0cdc5d2005d6f5116fd3d32af0
Student <LAB-106@aptech-msg.edu>
2024-02-08 23:32:10 -0800

    add new files

--- task.js
+++ task.js
@@ -1,4 +1,13 @@
-function increment(){
-    let i=document.getElementById("head").value;
-    console.log(i)
+let num=0;
+function inc(){
+    document.querySelector("h2").innerHTML=num=num+1;
+}
+function dec(){
+    if(num==0){
+        num=0;
+        document.querySelector("h2").innerHTML=num;
+    }
+    else{
+        document.querySelector("h2").innerHTML=num=num-1;
+    }
 }
